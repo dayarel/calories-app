e33c2a5e5d3554b24603ba240b447b6249832769
David Cortes <dayareldev@gmail.com>
2025-09-26 14:56:19 +0200

    Componente Form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,3 +1,89 @@
+import { useState } from "react";
+import { categories } from "../data/categories";
+import type { ChangeEvent, FormEvent } from "react";
+import type { Activity } from "../types";
+
 export const Form = () => {
-  return <div>form</div>;
+  const [activity, setActivity] = useState<Activity>({
+    category: 1,
+    name: "",
+    calories: 0,
+  });
+
+  const handleChange = (
+    e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>
+  ) => {
+    const isNumberField = ["category", "calories"].includes(e.target.id);
+
+    setActivity({
+      ...activity,
+      [e.target.id]: isNumberField ? +e.target.value : e.target.value,
+    });
+  };
+
+  const isValidActivity = () => {
+    const { name, calories } = activity;
+
+    return name.trim() != "" && calories > 0;
+  };
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  return (
+    <form
+      className="bg-white space-y-5 shadow rounded-lg p-10"
+      onSubmit={handleSubmit}
+    >
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="category">Categoría:</label>
+        <select
+          id="category"
+          className="border border-slate-300 bg-white w-fill p-2 rounded-lg"
+          value={activity.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="name" className="font-bold">
+          Actividad:
+        </label>
+        <input
+          id="name"
+          type="text"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, Pesas, Bicicleta"
+          value={activity.name}
+          onChange={handleChange}
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-3">
+        <label htmlFor="calories" className="font-bold">
+          Calorías:
+        </label>
+        <input
+          id="calories"
+          type="number"
+          className="border border-slate-300 p-2 rounded-lg"
+          placeholder="Ej. 300,500"
+          value={activity.calories}
+          onChange={handleChange}
+        />
+      </div>
+      <input
+        type="submit"
+        className="bg-gray-800 hover:bg-gray-900 disabled:opacity-10 w-full p-2 font-bold uppercase text-white cursor-pointer"
+        value={activity.category === 1 ? "Guardar Comida" : "Guardar Ejercicio"}
+        disabled={!isValidActivity()}
+      />
+    </form>
+  );
 };
